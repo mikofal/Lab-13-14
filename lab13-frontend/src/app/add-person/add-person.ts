e8516d911b64eec2f96c11e6d297933483371ff0
mikofal <154383387+mikofal@users.noreply.github.com>
2026-01-02 22:36:47 +0100

    lab14

--- lab13-frontend/src/app/add-person/add-person.ts
+++ lab13-frontend/src/app/add-person/add-person.ts
@@ -5,10 +5,15 @@ import { Router } from '@angular/router';
 import { PersonService } from '../person';
 import { Person } from '../person.model';
 
+import { MatInputModule } from '@angular/material/input';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatButtonModule } from '@angular/material/button';
+import { MatCardModule } from '@angular/material/card';
+
 @Component({
   selector: 'app-add-person',
   standalone: true,
-  imports: [CommonModule, FormsModule],
+  imports: [CommonModule, FormsModule, MatInputModule, MatFormFieldModule, MatButtonModule, MatCardModule],
   templateUrl: './add-person.html'
 })
 export class AddPersonComponent {
